b00ed1f3eb89fc48fc55d5c43da3687adba8b9d8
Sviatoslav Murzin <mursvyats@gmail.com>
2025-05-30 13:14:36 +0500

    refactor: refactored runme.sh

--- task3/runme.sh
+++ task3/runme.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+IN=/tmp/io
+OUT=/tmp/out
+CONF=/tmp/myconf
+LOGS=/tmp/myinit.log
+
 echo "Build myinit"
 make
 
 echo "Создание IO files"
-touch /tmp/in
-touch /tmp/out
+touch $IN
+touch $OUT
 
 echo "Создаем файл конфигурации"
-touch /tmp/myconf
+touch $CONF
 
 echo "Завершение остальных процессов myinit"
-killall myinit
+killall myinit || echo ""
 
-echo /bin/sleep 3 /tmp/in /tmp/out >/tmp/myconf
-echo /bin/sleep 5 /tmp/in /tmp/out >>/tmp/myconf
-echo /bin/sleep 10 /tmp/in /tmp/out >>/tmp/myconf
+echo /bin/sleep 3 $IN $OUT >/tmp/myconf
+echo /bin/sleep 5 $IN $OUT >>/tmp/myconf
+echo /bin/sleep 10 $IN $OUT >>/tmp/myconf
 
 echo "Запуск myinit"
-./myinit -c /tmp/myconf
+./myinit -c $CONF
 MYINIT_PID=$(pgrep myinit)
 echo $MYINIT_PID
 sleep 1
@@ -36,7 +41,7 @@ ps --ppid $MYINIT_PID -o cmd,pid >>result.txt
 echo "Ожидание завершения остальных процессов" >>result.txt
 sleep 10
 
-echo /bin/sleep 3 /tmp/in /tmp/out >/tmp/myconf
+echo /bin/sleep 3 $IN $OUT >/tmp/myconf
 
 killall -HUP myinit
 
@@ -47,6 +52,6 @@ ps --ppid $MYINIT_PID -o cmd,pid >>result.txt
 echo "" >>result.txt
 echo "========LOGS========" >>result.txt
 echo "" >>result.txt
-cat /tmp/myinit.log >>result.txt
+cat $LOGS >>result.txt
 
 killall myinit
